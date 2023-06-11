6d2f0c0ccf53d6485c0fe8da0a306e6ae6ab6a18
Sehyun Chung <sehyun.chung@icloud.com>
2023-06-11 15:52:43 +0900

    use morning for colorscheme

--- init.lua
+++ init.lua
@@ -18,7 +18,8 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "morning",
+  background = "light", -- "dark" or "light"
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
