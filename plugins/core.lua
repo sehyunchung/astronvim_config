8564be6eaba56b14bbb789956bcf2af064e1fd80
Sehyun Chung <sehyun.chung@icloud.com>
2023-06-12 19:26:45 +0900

    show hidden items

--- plugins/core.lua
+++ plugins/core.lua
@@ -20,6 +20,18 @@ return {
       return opts
     end,
   },
+  {
+  "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem ={
+        filtered_items = {
+          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+      }
+    }
+  }
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
   --
