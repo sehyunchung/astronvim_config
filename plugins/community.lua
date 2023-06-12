25d4eac49c907b74daf9b6083a69e89a0a2a8ebc
Sehyun Chung <sehyun.chung@icloud.com>
2023-06-12 19:27:09 +0900

    add some community plugins for motion

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,7 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.color.vim-highlighter" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.leap-nvim" },
 }
